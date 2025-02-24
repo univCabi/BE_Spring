e7f0554f871ddf1dc6ce94cd639c08ad364bcb48
yemin4242 <whereismysejong>
2025-02-24 19:01:00 +0900

    first commit

---
+++ src/main/java/org/univcabi/univcabi/UnivCabiApplication.java
@@ -0,0 +1,13 @@
+package org.univcabi.univcabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnivCabiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnivCabiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/univcabi/univcabi/UnivCabiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.univcabi.univcabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnivCabiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
