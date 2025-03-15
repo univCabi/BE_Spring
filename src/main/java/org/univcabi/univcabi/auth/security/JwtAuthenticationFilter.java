64bc61a99bf34a457da2d50e4eb05fd1864fc52f
민웅기 <d34sda334@gmail.com>
2025-03-15 16:21:24 +0900

    Feat: JWT 인증 피렅 구현#8

--- src/main/java/org/univcabi/univcabi/auth/security/JwtAuthenticationFilter.java
+++ src/main/java/org/univcabi/univcabi/auth/security/JwtAuthenticationFilter.java
@@ -1,4 +1,51 @@
 package org.univcabi.univcabi.auth.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException{
+
+        String token = resolveToken(request);
+
+        if(token != null && jwtTokenProvider.validateToken(token)){
+            String studentNumber = jwtTokenProvider.getStudentNumberFromToken(token);
+            UserDetails userDetails = userDetailsService.loadUserByUsername(studentNumber);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.info("JWT 인증 성공 {}",studentNumber);
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+    private String resolveToken(HttpServletRequest request){
+        String bearerToken = request.getHeader("Authorization");
+        if(bearerToken != null && bearerToken.startsWith("Bearer ")){
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
 }
