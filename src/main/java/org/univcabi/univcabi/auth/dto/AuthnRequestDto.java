db1fca3f23d3939d74df76a421f513c1719f1287
민웅기 <d34sda334@gmail.com>
2025-03-15 21:04:44 +0900

    Fix:RequestDto JSON 데이터 객체 변환 오류 해결#8

--- src/main/java/org/univcabi/univcabi/auth/dto/AuthnRequestDto.java
+++ src/main/java/org/univcabi/univcabi/auth/dto/AuthnRequestDto.java
@@ -1,7 +1,8 @@
 package org.univcabi.univcabi.auth.dto;
 
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,4 +13,11 @@ import lombok.NoArgsConstructor;
 public class AuthnRequestDto {
     private String studentNumber;
     private String password;
+
+    @JsonCreator
+    public AuthnRequestDto(@JsonProperty("studentNumber") String studentNumber,
+                           @JsonProperty("password") String password) {
+        this.studentNumber = studentNumber;
+        this.password = password;
+    }
 }
