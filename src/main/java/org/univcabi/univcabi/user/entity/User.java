380b777764d235f3b7717f4f694bf90584d199ac
yeomin4242 <whereismysejong@naver.com>
2025-03-24 19:07:45 +0900

    Feat: User 도메인 구현 #10

--- src/main/java/org/univcabi/univcabi/user/entity/User.java
+++ src/main/java/org/univcabi/univcabi/user/entity/User.java
@@ -13,10 +13,11 @@ import java.time.LocalDateTime;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.univcabi.univcabi.auth.entity.Authn;
 import org.univcabi.univcabi.cabinet.entity.Building;
 
 @Entity
-@Table(name = "users")  // DB의 'users' 테이블과 매핑
+@Table(name = "users")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Log4j2
 @Getter
@@ -26,16 +27,28 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @NotNull
-    @Column(nullable = false, length = 10)
+    @Column(nullable = false, length = 30)
     private String name;
 
-    @Enumerated(EnumType.STRING)
-    private Building building;
+    @Column(nullable = false)
+    private String affiliation;
+
+    @Column(name="phone_number")
+    private String phoneNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "building_id", nullable = false)
+    private Building building;  // 이것만 유지하고 다른 building 관련 필드는 제거
 
     @NotNull
     @Column(name="is_visible")
     private Boolean isVisible;
+
+    // User 엔티티는 관계의 주인이 아님 (mappedBy 사용)
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+    private Authn authn;
 
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
@@ -50,11 +63,14 @@ public class User {
 
 
     @Builder(toBuilder = true)
-    private User(Long id, String name, Building building, Boolean isVisible, LocalDateTime createdAt, LocalDateTime updatedAt,  LocalDateTime deletedAt) {
+    private User(Long id, String name, String affiliation, String phoneNumber, Building building, Boolean isVisible, Authn authn, LocalDateTime createdAt, LocalDateTime updatedAt, LocalDateTime deletedAt) {
         this.id = id;
         this.name = name;
+        this.affiliation = affiliation;
+        this.phoneNumber = phoneNumber;
         this.building = building;
         this.isVisible = isVisible;
+        this.authn = authn;
         this.createdAt = createdAt == null ? LocalDateTime.now() : createdAt;
         this.updatedAt = updatedAt == null ? LocalDateTime.now() : updatedAt;
         this.deletedAt = deletedAt;
