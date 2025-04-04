5d282065021dbbe27269ec7a7e47b33f4e4b2d6c
민웅기 <d34sda334@gmail.com>
2025-04-04 20:01:42 +0900

    Refactor: User 예외 처리 리팩토링#11

--- src/main/java/org/univcabi/univcabi/exception/ExceptionStatus.java
+++ src/main/java/org/univcabi/univcabi/exception/ExceptionStatus.java
@@ -38,14 +38,20 @@ public enum ExceptionStatus {
 
     //USER
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다"),
+    USER_INVALID_STUDENT_NUMBER(HttpStatus.BAD_REQUEST, "잘못된 학번입니다."),
+    USER_INVALID_VISIBILITY(HttpStatus.BAD_REQUEST, "잘못된 공개 여부 설정입니다."),
+    USER_VISIBILITY_UPDATE_FAILED(HttpStatus.BAD_REQUEST, "해당 유저정보를 수정할 수 없습니다."),
 
     // GENERAL ERROR CODE
     GENERAL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "서버에 잘못된 요청입니다."),
     GENERAL_REQUEST_INVALID_PARAMS(HttpStatus.BAD_REQUEST, "서버에 잘못된 요청입니다."),
 
+    SQL_FILE_LOAD_FAILED(HttpStatus.INTERNAL_SERVER_ERROR,"SQL 파일 로딩 실패."),
+
     GENERAL_INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 오류가 발생했습니다"),
     GENERAL_SERVICE_UNAVAILABLE(HttpStatus.SERVICE_UNAVAILABLE, "서버가 작동하지 않고 있습니다."),
     GENERAL_GATEWAY_TIMEOUT(HttpStatus.GATEWAY_TIMEOUT, "서버에서 타임아웃이 발생했습니다");
+
 
     // 필드 선언은 열거형 상수 뒤에 위치
     private final int statusCode;
--- src/main/java/org/univcabi/univcabi/user/repository/UserRepositoryImpl.java
+++ src/main/java/org/univcabi/univcabi/user/repository/UserRepositoryImpl.java
@@ -10,10 +10,14 @@ import org.univcabi.univcabi.cabinet.entity.CabinetHistory;
 import org.univcabi.univcabi.cabinet.entity.QBuilding;
 import org.univcabi.univcabi.cabinet.entity.QCabinet;
 import org.univcabi.univcabi.cabinet.entity.QCabinetHistory;
+import org.univcabi.univcabi.exception.ControllerException;
+import org.univcabi.univcabi.exception.RepositoryException;
 import org.univcabi.univcabi.user.entity.QUser;
 import org.univcabi.univcabi.user.entity.User;
 
 import java.util.Optional;
+
+import static org.univcabi.univcabi.exception.ExceptionStatus.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -24,13 +28,17 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     // StudentNumber에 해당 하는 User의 building, cabinet, cabinet_history 정보 조회
     @Override
     public Optional<CabinetHistory> getLatestCabinetHistoryByStudentNumber(String studentNumber){
+
+        if(studentNumber ==null) {
+            throw new ControllerException(USER_INVALID_STUDENT_NUMBER);
+        }
+
         QCabinetHistory history = QCabinetHistory.cabinetHistory;
         QCabinet cabinet = QCabinet.cabinet;
         QBuilding building = QBuilding.building;
         QUser user =QUser.user;
         QAuthn authn = QAuthn.authn;
 
-        // 무지성 join 나중에 수정 필요
         CabinetHistory result = queryFactory
                 .selectFrom(history)
                 .join(history.user, user).fetchJoin()
@@ -45,6 +53,7 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
                 .limit(1)  // 1개
                 .fetchOne();
 
+        // result 값이 Null인 경우도 정상임 -> 빌리고 있는 사물함이 없을 수 있음.
         return Optional.ofNullable(result);
     }
 
@@ -52,20 +61,36 @@ public class UserRepositoryImpl implements UserRepositoryCustom{
     // User의 정보 조회
     @Override
     public Optional<User> findUserByStudentNumber(String studentNumber){
+
+        if(studentNumber ==null) {
+            throw new ControllerException(USER_INVALID_STUDENT_NUMBER);
+        }
+
         QUser user = QUser.user;
         QAuthn authn =QAuthn.authn;
 
-        return Optional.ofNullable(
-                queryFactory
-                        .selectFrom(user)
-                        .join(user.authn,authn)
-                        .where(authn.studentNumber.eq(studentNumber))
-                        .fetchOne()
-        );
+        User result = queryFactory
+                .selectFrom(user)
+                .join(user.authn,authn)
+                .where(authn.studentNumber.eq(studentNumber))
+                .fetchOne();
+
+        if (result == null){
+            throw new RepositoryException(USER_NOT_FOUND);
+        }
+
+        return Optional.of(result);
     }
 
     @Override
     public long updateUserVisibilityByStudentNumber(String studentNumber, Boolean isVisible){
+
+        if(studentNumber == null) {
+            throw new ControllerException(USER_INVALID_STUDENT_NUMBER);
+        }else if(isVisible == null){
+            throw new ControllerException(USER_INVALID_VISIBILITY);
+        }
+
         QUser user = QUser.user;
         QAuthn authn =QAuthn.authn;
 
