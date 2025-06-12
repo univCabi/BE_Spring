74d98ae7ad179c05856b5e7b043f5668d57776d1
minwoonggi <d34sda334@gmail.com>
2025-06-12 20:56:00 +0900

    Refactor: Admin 사용자의 컨트롤러에 @PreAuthorize 인증 로직 추가#27

--- src/main/java/org/univcabi/univcabi/cabinet/controller/AdminCabinetController.java
+++ src/main/java/org/univcabi/univcabi/cabinet/controller/AdminCabinetController.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.univcabi.univcabi.cabinet.dto.response.CabinetStatusCountResponseDto;
@@ -18,6 +20,7 @@ import java.util.List;
 @RequestMapping("/cabinet/admin")
 @Tag(name = "ADMIN 사용자 사물함 관리 로직")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminCabinetController {
     private final CabinetService cabinetService;
 
@@ -32,4 +35,8 @@ public class AdminCabinetController {
         return ResponseEntity.ok(responseDto);
     }
 
+
+    @PostMapping("/return")
+    @Operation(summary = "반납할 사물함 id를 보내고 해당 사물함의 상태 값을 반환")
+    public ResponseEntity<?>
 }
